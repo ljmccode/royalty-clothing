db9c69fb843007432df9c1ad0e995f92e8c38466
Laura <lmcsay@gmail.com>
2022-07-13 15:13:17 -0500

    Add Crwn svg logo to Navigation route component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,19 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as CrwnLogo } from '../../assests/crown.svg';
+
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-        <div>Logo</div>
+          <CrwnLogo className='logo'></CrwnLogo>
         </Link>
         <div className='nav-links-container'></div>
-          <Link className='nav-link' to='/shop'>
-            SHOP
-          </Link>
+        <Link className='nav-link' to='/shop'>
+          SHOP
+        </Link>
         <Outlet />
       </div>
     </Fragment>
