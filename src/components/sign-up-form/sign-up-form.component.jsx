2fecb6374449816f969ab4453d93debca7c641c6
Laura <lmcsay@gmail.com>
2022-07-24 18:00:10 -0500

    Replace button element with button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 import './sign-up-form.styles.scss';
 
 import {
@@ -90,7 +91,7 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <button type='submit'>Sign Up</button>
+        <Button type='submit'>Sign Up</Button>
       </form>
     </div>
   );
