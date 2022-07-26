a13ae94737ccc5cc143f4055ee5bc186cb6bbf6b
Laura <lmcsay@gmail.com>
2022-07-26 14:38:00 -0500

    Create cart dropdown component and style

---
+++ src/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import Button from '../components/button/button.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+  return (
+    <div className='cart-dropdown-container show'>
+      <div className='cart-items'>
+        <p className='empty-message'>Your cart is empty</p>
+      </div>
+      <Button>Go to checkout</Button>
+    </div>
+  );
+};
+
+export default CartDropdown;
