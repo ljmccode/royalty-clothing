3dcc0079c34469c79be58bd2467fc3b4edc8c5ea
Laura <lmcsay@gmail.com>
2022-07-26 16:48:01 -0500

    Add items to cart by clicking button on product card

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,17 +1,29 @@
+import { useContext } from 'react';
+
 import Button from '../button/button.component';
+import { CartContext } from '../../contexts/cart.context';
 
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className='product-card-container'>
-      <div className='image' style={{backgroundImage : `url(${imageUrl})`}}></div>
+      <div
+        className='image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
       <div className='footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to Cart</Button>
+      <Button buttonType='inverted' onClick={addProductToCart}>
+        Add to Cart
+      </Button>
     </div>
   );
 };
