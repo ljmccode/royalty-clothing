00ed1bd03a2c99cd1dcaf8812da26f5cc75609c8
Laura <lmcsay@gmail.com>
2022-07-28 10:01:51 -0500

    Retrieve categories map from database on mount

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,21 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      await getCategoriesAndDocuments();
+    };
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
 
   return (
