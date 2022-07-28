b3f971eee66c2c6213c8a8723a7c401b9024dfd9
Laura <lmcsay@gmail.com>
2022-07-28 10:31:06 -0500

    Refactor shop page to show all shop categories and items within each category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, Fragment } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import ProductCard from '../../components/product-card/product-card.component';
@@ -7,12 +7,20 @@ import './shop.styles.scss';
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
+
   return (
-    <div className='products-container'>
-      {/* {categoriesMap.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))} */}
-    </div>
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className='products-container'>
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
