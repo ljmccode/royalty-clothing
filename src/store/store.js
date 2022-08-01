b74f7868f82936198945549eb9561ee46b546438
Laura <lmcsay@gmail.com>
2022-08-01 12:41:43 -0500

    Refactor store to use redux toolkit & configureStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,16 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
 const middleWares = [logger];
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(middleWares),
+  devTools: process.env.NODE_ENV !== 'production',
+});
