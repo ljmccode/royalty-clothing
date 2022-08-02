7235b1d5422b9da8655694ebe3f1b1f9586852d4
Laura <lmcsay@gmail.com>
2022-08-02 12:07:05 -0500

    Add cart reducer to root reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import userReducer from './user/user.reducer';
 import categoryReducer from './categories/category.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export const rootReducer = {
   user: userReducer,
   categories: categoryReducer,
+  cart: cartReducer,
 };
