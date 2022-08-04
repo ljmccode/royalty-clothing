9c4d9db3e111293abc09f2130c43ca8e0a830837
Laura <lmcsay@gmail.com>
2022-08-04 15:19:43 -0500

    Refactor async get categories call to use Redux Thunk

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,14 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchCategories } from './category.action';
 
 export const categorySlice = createSlice({
   name: 'categories',
   initialState: {
     categories: [],
+    isLoading: false,
+    error: null,
   },
   reducers: {
-    setCategories: (state, action) => {
-      state.categories = action.payload;
-    },
+    // setCategories: (state, action) => {
+    //   state.categories = action.payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCategories.fulfilled, (state, { payload }) => {
+      state.categories = payload;
+      state.isLoading = false;
+    });
+    builder.addCase(fetchCategories.pending, (state, { payload }) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchCategories.rejected, (state, { payload, error }) => {
+      state.isLoading = false;
+      state.categories = payload;
+      state.error = error;
+    });
   },
 });
 
