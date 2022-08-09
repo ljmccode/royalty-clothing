c47ee61048efb6ceefbcd6781114ebf97ec00ca2
Laura <lmcsay@gmail.com>
2022-08-09 12:44:21 -0500

    Refacotr email sign in start action to return email/password payload

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -4,7 +4,17 @@ export const checkUserSession = createAction('CHECK_USER_SESSION');
 
 export const googleSignInStart = createAction('GOOGLE_SIGN_IN_START');
 
-export const emailSignInStart = createAction('EMAIL_SIGN_IN_START');
+export const emailSignInStart = createAction(
+  'EMAIL_SIGN_IN_START',
+  function prepare(email, password) {
+    return {
+      payload: {
+        email,
+        password,
+      },
+    };
+  }
+);
 
 export const signInSuccess = createAction('SIGN_IN_SUCCESS');
 
