dfe317cd5d9b0f76041154dabb61ae7978f3a502
Laura <lmcsay@gmail.com>
2022-08-10 11:05:13 -0500

    Use client secret to confirm card payment with stripe

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,13 +1,60 @@
-import { CardElement } from '@stripe/react-stripe-js';
+import { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { useSelector } from 'react-redux';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
+import { PaymentFormContainer, FormContainer } from './payment-form.styles';
+
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Simon Cowell',
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Succeeded');
+      }
+    }
+  };
+
   return (
-    <div>
-      <CardElement />
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
-    </div>
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
   );
 };
 
