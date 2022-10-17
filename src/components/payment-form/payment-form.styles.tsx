91c01f93b06308024f520b5e0b6eb2c7e078bc83
Laura <lmcsay@gmail.com>
2022-10-17 14:04:39 -0500

    Make payment form mobile friendly

--- src/components/payment-form/payment-form.styles.tsx
+++ src/components/payment-form/payment-form.styles.tsx
@@ -7,16 +7,27 @@ export const PaymentFormContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 800px) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const FormContainer = styled.form`
   height: 100px;
   min-width: 500px;
+
+  @media screen and (max-width: 800px) {
+    min-width: unset;
+    width: 90vw;
+  }
 `;
 
 export const PaymentButton = styled(Button)`
   margin-left: auto;
   margin-top: 30px;
+
+  
 `;
 
 export const CardInputContainer = styled.div`
